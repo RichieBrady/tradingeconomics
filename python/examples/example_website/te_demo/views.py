a5aeabbea0f9f8896088f81828f67110945006ab
RichieBrady <richard.brady@ucdconnect.ie>
2023-04-28 20:59:41 +0100

    Add views, urls, and models for the listed country and category eurostat data. Save listed data to DB for easy access.

--- python/examples/example_website/te_demo/views.py
+++ python/examples/example_website/te_demo/views.py
@@ -1,6 +1,10 @@
+import json
+
+from django.db.models import F
 from django.http import JsonResponse
 from django.shortcuts import render
-from .te_api import te_login
+from .te_api import get_correct_data
+from .models import Country, Category
 
 
 # Create your views here.
@@ -8,8 +12,26 @@ def index(request):
     return render(request, 'te_demo/index.html')
 
 
+def get_countries_list(request):
+    countries = list(
+        Country.objects.all().annotate(Country=F('country')).values('Country')
+    )
+    return JsonResponse({'country_data': countries}, status=200)
+
+
+def get_categories_list(request):
+    categories = list(
+        Category.objects.all().annotate(Category=F('category'), CategoryGroup=F('category_group')).values('Category', 'CategoryGroup')
+    )
+    return JsonResponse({'category_data': categories}, status=200)
+
+
 def get_data(request):
     data = dict(request.GET)
-    te_login()
+    data = {key: value[0] for key, value in zip(data.keys(), data.values()) if value[0]}
+    if not data:
+        return JsonResponse({'msg': 'NOT_OK'}, status=200)
     print(data)
-    return JsonResponse({'msg': 'OK'}, status=200)
+    df_json, df_columns = get_correct_data(data)
+    return JsonResponse({'table_data': df_json, 'column_names': df_columns}, safe=False, status=200)
+
