380aeb10e1b18d68988467e8647a88fd026e09b1
RichieBrady <richard.brady@ucdconnect.ie>
2023-05-05 17:20:38 +0100

    Remove categories url to reflect changes in views

--- python/examples/example_website/te_demo/urls.py
+++ python/examples/example_website/te_demo/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("get_categories/", views.get_categories_list, name="get_categories"),
     path("get_countries/", views.get_countries_list, name="get_countries"),
     path("get_data/", views.get_data, name="get_data")
 ]
